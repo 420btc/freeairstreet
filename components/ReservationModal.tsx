8ba21a946aee585ad8585acffcdb302cd79d871e
420btc <chocolati_558@hotmail.com>
2025-08-09 00:49:14 +0200

    style(ReservationModal): adjust close button position and styling
    Make close button positioning more consistent with design system by adjusting top and right spacing

--- components/ReservationModal.tsx
+++ components/ReservationModal.tsx
@@ -140,9 +140,9 @@ export function ReservationModal({ isOpen, onClose, type, itemName, itemPrice, i
               variant="ghost"
               size="sm"
               onClick={handleClose}
-              className="text-white hover:bg-white/20 h-8 w-8 p-0 ml-4 flex-shrink-0 absolute top-4 right-4"
+              className="text-white hover:bg-white/20 h-8 w-8 sm:h-8 sm:w-8 md:h-8 md:w-8 p-0 ml-4 flex-shrink-0 absolute top-3 right-6"
             >
-              <X className="h-4 w-4" />
+              <X className="h-5 w-5 sm:h-4 sm:w-4" />
             </Button>
           </div>
         </CardHeader>
